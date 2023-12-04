7f1275b9b50749e843831a5160d281b8d0d5c06e
salla <sallakos@hotmail.com>
2023-12-04 11:42:37 +0200

    add performance log

--- 2023/utils.js
+++ 2023/utils.js
@@ -10,3 +10,6 @@ export const sum = array => array.reduce((a, b) => a + b, 0)
 
 export const log = (part, text, value) =>
   console.log(`Part ${part}: ${text} is ${value}`)
+
+export const logPerformance = (start, end) =>
+  console.log(`took ${Math.round((end - start) * 100) / 100} ms`)
