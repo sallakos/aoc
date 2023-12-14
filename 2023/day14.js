b51b17735e65f7aab2cdc2744f29c04f245590b2
salla <sallakos@hotmail.com>
2023-12-14 18:00:14 +0200

    day 14 2023

---
+++ 2023/day14.js
@@ -0,0 +1,104 @@
+import { log, logPerformance, readFileToLines, sum } from './utils.js'
+const lines = readFileToLines('14')
+
+const LENGTH = lines.length
+
+const rotate = original => {
+  const rotated = []
+  original.forEach(o => {
+    rotated.push([o[1], LENGTH - 1 - o[0]])
+  })
+  return rotated
+}
+
+const sortByColumn = arr =>
+  arr.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0]
+    }
+    return a[1] - b[1]
+  })
+
+const roundRocks = []
+const cubeRocks = []
+
+lines.forEach((l, index) => {
+  for (let i = 0; i < l.length; i++) {
+    if (l.charAt(i) === 'O') roundRocks.push([index, i])
+    if (l.charAt(i) === '#') cubeRocks.push([index, i])
+  }
+})
+
+sortByColumn(roundRocks)
+
+const tiltNorth = (round, cube) => {
+  const newIndices = []
+  round.forEach(([row, column]) => {
+    const cubes = cube
+      .filter(([cRow, cColumn]) => cColumn === column)
+      .map(([cRow]) => cRow)
+    const rounds = newIndices
+      .filter(([cRow, cColumn]) => cColumn === column)
+      .map(([cRow]) => cRow)
+
+    const lowestPossible = Math.max(
+      Math.max(...cubes.concat(rounds).filter(c => c < row)) + 1,
+      0
+    )
+    newIndices.push([lowestPossible, column])
+  })
+  return newIndices
+}
+
+// Part 1
+log(
+  1,
+  'total load on the north support beams',
+  sum(tiltNorth(roundRocks, cubeRocks).map(r => LENGTH - r[0]))
+)
+
+// Part 2
+const start = performance.now()
+const cycle = (round, cube) => {
+  let r = round
+  let c = cube
+  for (let i = 0; i < 4; i++) {
+    r = sortByColumn(rotate(tiltNorth(r, c)))
+    c = rotate(c)
+  }
+  return sortByColumn(r)
+}
+
+let toCycle = roundRocks
+let occured = new Map()
+const roundIndices = [toCycle]
+let i = 1
+let repetitionStart
+let repetitionLength
+
+while (i <= 1000000000) {
+  toCycle = cycle(toCycle, cubeRocks)
+  const rString = toCycle.toString()
+  if (occured.has(rString)) {
+    const index = occured.get(rString)
+    repetitionStart = index
+    repetitionLength = i - index
+    break
+  }
+  roundIndices.push(toCycle)
+  occured.set(rString, i)
+  i++
+}
+
+const cycledIndices =
+  roundIndices[
+    repetitionStart + ((1000000000 - repetitionStart) % repetitionLength)
+  ]
+
+const end = performance.now()
+log(
+  2,
+  'total load on the north support beams after 1 000 000 000 cycles',
+  sum(cycledIndices.map(s => LENGTH - s[0]))
+)
+logPerformance(start, end, true)
